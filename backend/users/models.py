32a9e5f327824e294b687ba454fce4f0b78c978b
Seniacat <senya.sidorovich@gmail.com>
2022-03-20 21:02:46 +0300

    added users app, recipes app

---
+++ backend/users/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    email = models.EmailField(
+        'email',
+        unique=True,
+        max_length=254
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural ='Пользователи'
