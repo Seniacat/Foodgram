954d382934ac328b3014474a9e58e4c67fafb3f9
Seniacat <senya.sidorovich@gmail.com>
2022-04-13 17:53:27 +0300

    docker-compose

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -1,11 +1,10 @@
-import django_filters
-from django_filters import filters
+from django_filters.rest_framework import FilterSet, filters
 
 from users.models import User
 from recipes.models import Recipe
 
 
-class TagFilter(django_filters.FilterSet):
+class TagFilter(FilterSet):
     author = filters.ModelChoiceFilter(queryset=User.objects.all())
     tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
     is_favorited = filters.BooleanFilter(method='get_is_favorited')
@@ -17,11 +16,12 @@ class TagFilter(django_filters.FilterSet):
         fields = ('tags', 'author','is_favorited', 'is_in_shopping_cart')
 
     def get_is_favorited(self, queryset, name, value):
-        if value:
-            return queryset.filter(favorites__user=self.request.user)
+        print(self.request.user, value)
+        if self.request.user.is_authenticated and value is True:
+            return queryset.filter(users_favorites__user=self.request.user)
         return queryset
 
     def get_is_in_shopping_cart(self, queryset, name, value):
-        if value:
+        if self.request.user.is_authenticated and value is True:
             return queryset.filter(shopping_cart__user=self.request.user)
         return queryset
--- backend/recipes/management/commands/import_ings.py
+++ backend/recipes/management/commands/import_ings.py
@@ -1,6 +1,8 @@
 import csv
+import os
 import logging
 
+from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 from recipes.models import Ingredient
 
@@ -9,27 +11,38 @@ logging.basicConfig(
     filename='main.log',
     format='%(asctime)s, %(levelname)s, %(name)s, %(message)s',
     filemode='w',
-    encoding='utf-8'
 )
 
-CSV_PATH = '../data/ingredients.csv'
+DATA_ROOT = os.path.join(settings.BASE_DIR, 'data') 
 
 
 class Command(BaseCommand):
     help = 'Load data from csv file into the database'
 
-    def handle(self, *args, **kwargs):
+    def add_arguments(self, parser):
+        parser.add_argument('filename', default='ingredients.csv', nargs='?',
+                            type=str)
+
+    def handle(self, *args, **options):
         try:
             with open(
-                CSV_PATH,
+                os.path.join(DATA_ROOT, options['filename']),
                 newline='',
                 encoding='utf8'
             ) as csv_file:
-                fieldnames = ['name', 'measurement_unit']
+
+                """fieldnames = ['name', 'measurement_unit']
                 reader = csv.DictReader(csv_file, fieldnames=fieldnames)
                 Ingredient.objects.bulk_create(
                                 Ingredient(**data) for data in reader
-                )
+                )"""
+                data = csv.reader(csv_file)
+                for row in data:
+                    name, measurement_unit = row
+                    Ingredient.objects.get_or_create(
+                        name=name,
+                        measurement_unit=measurement_unit
+                    )
         except Exception as error:
             CommandError(error)
         logging.info('Successfully loaded all data into database')
