d53fd3131d14c2ff25b3987fd384fb1d70d89ec5
Seniacat <senya.sidorovich@gmail.com>
2022-04-16 14:11:03 +0300

    Пагинация

--- backend/recipes/pagination.py
+++ backend/recipes/pagination.py
@@ -4,3 +4,5 @@ from rest_framework.pagination import PageNumberPagination
 class CustomPagination(PageNumberPagination):
     page_size = 6
     page_size_query_param = 'limit'
+    max_page_size = 6
+    page_query_param = 'page'
