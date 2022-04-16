e52f61200b8aad2e30e1ebe45859a00788ba70ed
Seniacat <senya.sidorovich@gmail.com>
2022-04-16 13:34:07 +0300

    Добавлена пагинация

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -148,7 +148,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'recipes.pagination.CustomPagination',
+    'PAGE_SIZE': 6,
 }
 
 DJOSER = {
